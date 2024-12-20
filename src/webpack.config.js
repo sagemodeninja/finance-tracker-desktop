4037f009c64c45ab4300b8a7d823f2e1f7b39b82
Gary Antier <contact@garyantier.com>
2024-12-20 09:22:06 +0800

    feat: init typescript

---
+++ src/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const entries = require('./auto-entry')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = (_, {mode}) => {
+    const isDevelopment = mode === 'development'
+
+    return {
+        target: 'web',
+        entry: entries.load({ type: 'entry' }),
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'scripts/[name].js',
+            clean: true
+        },
+        plugins: [
+            ...entries.load({ type: 'view' }),
+            new MiniCssExtractPlugin({
+                filename : 'styles/[name].css'
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    // For *.component.scss
+                    test: /\.component\.s[ac]ss$/,
+                    use: [
+                        'css-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: { api: 'modern' }
+                        }
+                    ],
+                },
+                // For *.scss
+                {
+                    test: /\.s[ac]ss$/,
+                    exclude: /\.component\.s[ac]ss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: { api: 'modern' }
+                        }
+                    ],
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.js', '.scss'],
+            alias: {
+                '@': path.resolve(__dirname, 'scripts'),
+                '@styles': path.resolve(__dirname, 'styles'),
+            }
+        },
+        devtool: isDevelopment ? 'inline-source-map' : false
+    }
+}
